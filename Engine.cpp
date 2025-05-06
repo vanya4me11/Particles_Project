6cffe28b6bab3cd9f50469d1cc31c5344f02c9f0
Zerio <Zerio105@gmail.com>
2025-05-06 13:48:01 -0700

    Created Engine.cpp

---
+++ Engine.cpp
@@ -0,0 +1,25 @@
+#include "Engine.h"
+
+Engine::Engine()
+{
+	return;
+}
+
+// Run will call all the private functions
+void Engine::run()
+{
+	return;
+}
+
+void Engine::input()
+{
+	return;
+}
+void Engine::update(float dtAsSeconds)
+{
+	return;
+}
+void Engine::draw()
+{
+	return;
+}
