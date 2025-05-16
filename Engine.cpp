55daad8873c452a58252b7e49da1b7e27de8861d
BuildTools <unconfigured@null.spigotmc.org>
2025-05-15 18:10:39 -0700

    added J functionality, creates design

--- Engine.cpp
+++ Engine.cpp
@@ -132,13 +132,130 @@ void Engine::input()
 	}
 
 	if (jWasPressed) {
-		//for (int d_x = )
-		m_controllableParticle = new Particle(m_Window, 30, Vector2i(m_Window.getSize().x / 2, m_Window.getSize().y / 2));
-		m_particles.push_back(m_controllableParticle);
-		jWasPressed = true;
+		Vector2u winSize = m_Window.getSize();									// window size
+		float circleRadius = 300.f;												// r
+		float circleYOffset = 150.f;
+
+		
+		Vector2f center(winSize.x / 2.f, winSize.y / 2.f - circleYOffset);		//artificial center (mapping purposes)
+
+		// circle
+		int numCircleParticles = 18;
+		for (int i = 0; i < numCircleParticles; ++i) {
+			float angle = i * (2 * M_PI / numCircleParticles);
+			float x = center.x + circleRadius * cos(angle);
+			float y = center.y + circleRadius * sin(angle);
+			m_particles.push_back(new WaveParticle(m_Window, 25, Vector2i((int)x, (int)y)));
+		}
+
+		// horizontal line
+		int numParticlesX = 40;
+		float spacingX = winSize.x / (float)(numParticlesX + 1);
+		for (int i = 1; i <= numParticlesX; ++i) {
+			float x = i * spacingX;
+			float y = center.y;
+
+			float dx = x - center.x;
+			float dy = y - center.y;
+			float dist = sqrt(dx * dx + dy * dy);
+			if (dist >= circleRadius + 5) { // +5 buffer to leave gap
+				m_particles.push_back(new WaveParticle(m_Window, 30, Vector2i((int)x, (int)y)));
+			}
+		}
+
+		// vertical line
+		int numParticlesY = 30;
+		float spacingY = winSize.y / (float)(numParticlesY + 1);
+		for (int i = 1; i <= numParticlesY; ++i) {
+			float x = center.x;
+			float y = i * spacingY;
+
+			float dx = x - center.x;
+			float dy = y - center.y;
+			float dist = sqrt(dx * dx + dy * dy);
+			if (dist >= circleRadius + 5) {
+				m_particles.push_back(new WaveParticle(m_Window, 30, Vector2i((int)x, (int)y)));
+			}
+		}
+
+		// 5 petal rose curve
+		int numRoseParticles = 50;  // less is more
+		float roseRadius = 150.f;    // < circle radius
+		int k = 5;                   // 5 petals
+
+		for (int i = 0; i < numRoseParticles; ++i) {
+			float theta = i * (2 * M_PI / numRoseParticles); // angle step
+			float r = roseRadius * cos(k * theta);           // rose equation
+
+			float x = center.x + r * cos(theta);
+			float y = center.y + r * sin(theta);
+
+			m_particles.push_back(new WaveParticle(m_Window, 25, Vector2i((int)x, (int)y)));
+		}
+
+		// rectangle shape thingy
+		float rectWidth = 80.f;
+		float rectHeight = 300.f;
+		float baseLegLength = 40.f;
+		float baseLegHeight = 20.f;
+
+		// center of 4th quadrant (bottom-right)
+		float rectX = winSize.x * 0.75f - rectWidth / 2.f;
+		float rectY = winSize.y * 0.75f - rectHeight / 2.f;
+
+		// center for 3rd quadrant
+		float dRectX = winSize.x * 0.25f - rectWidth / 2.f;  // X shifted to left quarter
+		float dRectY = winSize.y * 0.75f - rectHeight / 2.f; // Y stays in bottom half
+		Vector2f drectTopCenter(dRectX + rectWidth / 2.f, dRectY + 40.f);
+
+
+		// vahjra heart design
+		int heartPoints = 64;  // 
+		float scale = 100.f;
+
+		for (int i = 0; i < heartPoints; ++i) {
+			float theta = i * (2 * M_PI / heartPoints);
+			// Polar heart equation
+			float r = scale * (1 - sin(theta));
+
+			float x = drectTopCenter.x + r * cos(theta);
+			float y = drectTopCenter.y + r * sin(theta);
+
+			m_particles.push_back(new ConstantParticle(m_Window, 20, Vector2i((int)x, (int)y)));
+		}
+
+		// rectangle border
+		int rectOutlinePoints = 6;
+		for (int i = 0; i < rectOutlinePoints; ++i) {
+			float t = i / (float)(rectOutlinePoints - 1);
+
+			// LHS
+			float xL = rectX;
+			float yL = rectY + t * rectHeight;
+			m_particles.push_back(new WaveParticle(m_Window, 20, Vector2i((int)xL, (int)yL)));
+
+			// RHS
+			float xR = rectX + rectWidth;
+			float yR = rectY + t * rectHeight;
+			m_particles.push_back(new WaveParticle(m_Window, 20, Vector2i((int)xR, (int)yR)));
+		}
+
+		// Top and bottom lines of the rectangle
+		for (int i = 0; i < rectOutlinePoints; ++i) {
+			float t = i / (float)(rectOutlinePoints - 1);
+			float x = rectX + t * rectWidth;
+
+			// Top
+			m_particles.push_back(new WaveParticle(m_Window, 20, Vector2i((int)x, (int)rectY)));
+
+			// Bottom
+			m_particles.push_back(new WaveParticle(m_Window, 20, Vector2i((int)x, (int)(rectY + rectHeight))));
+		}
+
+
+	}
 	}
 
-}
 
 // .:[Engine Logic / Physics Updates]:.
 void Engine::update(float dtAsSeconds)
